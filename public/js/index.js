0ab050373aff3772452db19cc3c4970d579eda7c
hung <ngtuonghung@gmail.com>
2023-12-15 00:21:52 +0700

    new update

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,72 @@
-import { getProducts } from "./services/product.services.js";
-getProducts().then(function (products) {
-  $.each(products, function (index, product) {
-    $(`
-          <div class="product">
-            <div class="product-thumbnail">
-              <img src="${product.thumbnail}" 
-              alt="${product.title}" />
-            </div>
-            <div class="product-infor">
-              <h3 class="product-title">${product.title}</h3>
-            </div>
-          </div>
-        `).appendTo(".products");
+// import { getProducts } from "./services/product.services.js";
+// getProducts().then(function (products) {
+//   $.each(products, function (index, product) {
+//     $(`
+//           <div class="product">
+//             <div class="product-thumbnail">
+//               <img src="${product.thumbnail}"
+//               alt="${product.title}" />
+//             </div>
+//             <div class="product-infor">
+//               <h3 class="product-title">${product.title}</h3>
+//             </div>
+//           </div>
+//         `).appendTo(".products");
+//   });
+// });
+// document.addEventListener("DOMContentLoaded", function () {
+//   const overlayLeft = document.getElementById("overlay-left");
+//   const toggleOverlay = document.getElementById("toggle-overlay");
+
+//   toggleOverlay.addEventListener("click", function () {
+//     overlayLeft.classList.toggle("active");
+//   });
+
+//   // Tự động tắt lớp khi bấm ra ngoài
+//   window.addEventListener("click", function (event) {
+//     if (
+//       !overlayLeft.contains(event.target) &&
+//       !toggleOverlay.contains(event.target)
+//     ) {
+//       overlayLeft.classList.remove("active");
+//     }
+//   });
+// });
+// document.addEventListener("DOMContentLoaded", function () {
+//   var overlay = document.getElementById("overlay");
+
+//   // Hiển thị overlay
+//   function showOverlay() {
+//     overlay.style.display = "block";
+//   }
+
+//   // Ẩn overlay
+//   function hideOverlay() {
+//     overlay.style.display = "none";
+//   }
+
+//   // Lắng nghe sự kiện click trên body
+//   document.body.addEventListener("click", function (event) {
+//     // Kiểm tra xem sự kiện click có xảy ra bên ngoài overlay không
+//     if (!overlay.contains(event.target)) {
+//       hideOverlay();
+//     }
+//   });
+
+//   // Hiển thị overlay khi cần
+//   showOverlay();
+// });
+document.addEventListener("DOMContentLoaded", function () {
+  var overlay = document.getElementById("overlay");
+
+  // Hiển thị lớp ảo khi người dùng nhấp vào FLOWERS
+  document.querySelector(".nav").addEventListener("click", function (event) {
+    event.preventDefault();
+    overlay.style.display = "block";
+  });
+
+  // Ẩn lớp ảo khi người dùng nhấp vào nó
+  overlay.addEventListener("click", function () {
+    overlay.style.display = "none";
   });
 });
--- public/js/main.js
+++ public/js/main.js
@@ -13,3 +13,49 @@ function increaseValue() {
 
   quantityInput.value = currentValue + 1;
 }
+// document.querySelector(".overlay-left").addEventListener("click", function () {
+//   document.querySelector(".overlay-container").classList.toggle("show");
+// });
+// document.addEventListener("DOMContentLoaded", function () {
+//   const overlayLeft = document.getElementById("overlay-left");
+//   const toggleOverlay = document.getElementById("toggle-overlay");
+
+//   toggleOverlay.addEventListener("click", function () {
+//     overlayLeft.classList.toggle("active");
+//   });
+
+//   // Tự động tắt lớp khi bấm ra ngoài
+//   window.addEventListener("click", function (event) {
+//     if (
+//       !overlayLeft.contains(event.target) &&
+//       !toggleOverlay.contains(event.target)
+//     ) {
+//       overlayLeft.classList.remove("active");
+//     }
+//   });
+// });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   var overlay = document.getElementById("overlay");
+
+//   // Hiển thị overlay
+//   function showOverlay() {
+//     overlay.style.display = "block";
+//   }
+
+//   // Ẩn overlay
+//   function hideOverlay() {
+//     overlay.style.display = "none";
+//   }
+
+//   // Lắng nghe sự kiện click trên body
+//   document.body.addEventListener("click", function (event) {
+//     // Kiểm tra xem sự kiện click có xảy ra bên ngoài overlay không
+//     if (!overlay.contains(event.target)) {
+//       hideOverlay();
+//     }
+//   });
+
+//   // Hiển thị overlay khi cần
+//   showOverlay();
+// });
