0752283804485c7d6b8abc576294d40d913348fe
hung <ngtuonghung@gmail.com>
2023-12-21 02:39:48 +0700

    update

--- public/js/index.js
+++ public/js/index.js
@@ -1,61 +1,3 @@
-// import { getProducts } from "./services/product.services.js";
-// getProducts().then(function (products) {
-//   $.each(products, function (index, product) {
-//     $(`
-//           <div class="product">
-//             <div class="product-thumbnail">
-//               <img src="${product.thumbnail}"
-//               alt="${product.title}" />
-//             </div>
-//             <div class="product-infor">
-//               <h3 class="product-title">${product.title}</h3>
-//             </div>
-//           </div>
-//         `).appendTo(".products");
-//   });
-// });
-// document.addEventListener("DOMContentLoaded", function () {
-//   const overlayLeft = document.getElementById("overlay-left");
-//   const toggleOverlay = document.getElementById("toggle-overlay");
-
-//   toggleOverlay.addEventListener("click", function () {
-//     overlayLeft.classList.toggle("active");
-//   });
-
-//   // Tự động tắt lớp khi bấm ra ngoài
-//   window.addEventListener("click", function (event) {
-//     if (
-//       !overlayLeft.contains(event.target) &&
-//       !toggleOverlay.contains(event.target)
-//     ) {
-//       overlayLeft.classList.remove("active");
-//     }
-//   });
-// });
-// document.addEventListener("DOMContentLoaded", function () {
-//   var overlay = document.getElementById("overlay");
-
-//   // Hiển thị overlay
-//   function showOverlay() {
-//     overlay.style.display = "block";
-//   }
-
-//   // Ẩn overlay
-//   function hideOverlay() {
-//     overlay.style.display = "none";
-//   }
-
-//   // Lắng nghe sự kiện click trên body
-//   document.body.addEventListener("click", function (event) {
-//     // Kiểm tra xem sự kiện click có xảy ra bên ngoài overlay không
-//     if (!overlay.contains(event.target)) {
-//       hideOverlay();
-//     }
-//   });
-
-//   // Hiển thị overlay khi cần
-//   showOverlay();
-// });
 document.addEventListener("DOMContentLoaded", function () {
   var overlay = document.getElementById("overlay");
 
@@ -70,3 +12,4 @@ document.addEventListener("DOMContentLoaded", function () {
     overlay.style.display = "none";
   });
 });
+//
